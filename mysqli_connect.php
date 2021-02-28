676758d6ff14cacdf2d3d2a151f187af29e47377
Tushar Khatri <tkhatri@conestogac.on.ca>
2021-02-28 14:07:19 -0500

    Update mysqli_connect.php

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -4,7 +4,7 @@
    define('DB_USER', 'tusharPHPWinter2021');
    define('DB_PASSWORD','tushar');
    define('DB_HOST','localhost');
-   define('DB_NAME','task6');
+   define('DB_NAME','bookstoreschema');
 
    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) 
                     OR die('could not connect ' . mysqli_connect_error());
