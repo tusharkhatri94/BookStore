9604da45af31819d72eefaba8ba56e367aa5154a
Tushar Khatri <tkhatri@conestogac.on.ca>
2021-02-28 14:03:55 -0500

    Create mysqli_connect.php

---
+++ mysqli_connect.php
@@ -0,0 +1,13 @@
+<?php 
+
+   # mysqli_connect(hostname, username, password, databasename);
+   define('DB_USER', 'tusharPHPWinter2021');
+   define('DB_PASSWORD','tushar');
+   define('DB_HOST','localhost');
+   define('DB_NAME','task6');
+
+   $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) 
+                    OR die('could not connect ' . mysqli_connect_error());
+
+   mysqli_set_charset($connection,'utf8');
+?>
